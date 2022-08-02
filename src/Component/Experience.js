eeafc6cfd05b15a7e33db6a56f8c1af3febdc402
Akash B <akashbadole@rediffmail.com>
2022-08-02 21:02:31 +0530

    changes - content

--- src/Component/Experience.js
+++ src/Component/Experience.js
@@ -15,7 +15,7 @@ class Experience extends Component {
                     <div className="accordion-item">
                         <h2 className="accordion-header" id="flush-headingOne">
                             <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                            ANI TECHNOLOGIES PVT LTD— WordPress Developer & Front End Developer
+                            ANI TECHNOLOGIES PVT LTD— WordPress Developer, Front End Developer, Zendesk
                             </button>
                         </h2>
                         <div id="flush-collapseOne" className="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
@@ -36,7 +36,7 @@ class Experience extends Component {
                     <div className="accordion-item">
                         <h2 className="accordion-header" id="flush-headingTwo">
                             <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-                            SA TECHNOLOGIES, PUNE— WordPress Developer & Front End Developer
+                            SA TECHNOLOGIES, PUNE— WordPress Developer & Front End Developer,
                             </button>
                         </h2>
                         <div id="flush-collapseTwo" className="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
@@ -57,7 +57,7 @@ class Experience extends Component {
                     <div className="accordion-item">
                         <h2 className="accordion-header" id="flush-headingThree">
                             <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-                            SMARTCOOKIE REWARD PVT LTD, PUNE— WordPress Developer & Front End Developer
+                            SMARTCOOKIE REWARD PVT LTD, PUNE— WordPress Developer & Front End Developer,
                             </button>
                         </h2>
                         <div id="flush-collapseThree" className="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
@@ -81,7 +81,7 @@ class Experience extends Component {
                     <div className="accordion-item">
                         <h2 className="accordion-header" id="flush-headingFour">
                             <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFour" aria-expanded="false" aria-controls="flush-collapseFour">
-                            CLIDE MANAGEMENT CONSULTANCY PVT LTD , Nagpur — WordPress Developer & Front End Developer
+                            CLIDE MANAGEMENT CONSULTANCY PVT LTD , Nagpur — WordPress Developer & Front End Developer,
                             </button>
                         </h2>
                         <div id="flush-collapseFour" className="accordion-collapse collapse" aria-labelledby="flush-headingFour" data-bs-parent="#accordionFlushExample">
